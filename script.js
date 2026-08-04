b4d5ab08e1a40b9dff8775ceedc48111724ff5e3
syntaxash <sharmaashish29012005@gmail.com>
2026-08-05 00:51:37 +0530

    collision done

--- script.js
+++ script.js
@@ -1,38 +1,71 @@
 let character = document.querySelector("#character");
+let laboratory = document.querySelector("#laboratory");
+let dialog_box = doucument.querySelector("dialog_box");
+
+
+//keydown functions
 
 document.addEventListener("keydown", (event) => {
-    if (event.key === 'w'||
-        event.key === 'W'||
-        event.key ==='ArrowUp') {
+    if (event.key === 'w' ||
+        event.key === 'W' ||
+        event.key === 'ArrowUp') {
         gsap.to("#character", {
             y: "-=30",
             duration: 0.5
         });
     }
-    else if (event.key === 's'||
-             event.key === 'S'||
-             event.key ==='ArrowDown') {
+    else if (event.key === 's' ||
+        event.key === 'S' ||
+        event.key === 'ArrowDown') {
         gsap.to("#character", {
             y: "+=30",
             duration: 0.5
         });
     }
-    else if (event.key === 'a'||
-             event.key === 'A'||
-             event.key ==='ArrowLeft') {
+    else if (event.key === 'a' ||
+        event.key === 'A' ||
+        event.key === 'ArrowLeft') {
         gsap.to("#character", {
             x: "-=30",
             duration: 0.5
         });
     }
-    else if (event.key === 'd'||
-             event.key === 'D'||
-             event.key ==='ArrowRight') {
+    else if (event.key === 'd' ||
+        event.key === 'D' ||
+        event.key === 'ArrowRight') {
         gsap.to("#character", {
             x: "+=30",
             duration: 0.5
         });
     }
+    //page switch
+    else if (event.key === 'e' ||
+        event.key === 'E' && overlapping == true) {
+        window.loaction.href = 'laboratory.html';
+    }
+
 });
+
+//cheecking overlap
+
+let box1 = character.getBoundingClientReact();
+let box2 = laboratory.getBoundingClientReact();
+
+let overlapping = false;
+
+if (
+    box1.right < box2.right ||
+    box1.left < box2.left ||
+    box1.top < box2.top ||
+    box1.bottom < box2.bottom) {
+
+    let overlapping = true;
+}
+
+if (overlapping != true) {
+    dialog_box.style.visibility = "hidden";
+}
+
+
 
 
