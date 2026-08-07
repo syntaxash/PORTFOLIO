831cc6cb1a1db3486c14dac05870d240081e97a1
syntaxash <sharmaashish29012005@gmail.com>
2026-08-08 01:10:53 +0530

    adding laboratory

--- laboratory.js
+++ laboratory.js
@@ -1,13 +1,16 @@
-let character = document.querySelector("#character2");
-let flask = document.querySelector("#flask");
-let popup = document.querySelector("#popup");
+let character = document.querySelector("#character");
+let laboratory = document.querySelector("#flask1");
+let dialog_box = document.querySelector("#popup");
 
-let overlapping=false;
 
-function checkFlaskCollision() {
+//checking collision 
 
-    let box1 = character2.getBoundingClientRect();
-    let box2 = flask.getBoundingClientRect();
+let overlapping = false;
+
+function checkCollision() {
+
+    let box1 = character.getBoundingClientRect();
+    let box2 = laboratory.getBoundingClientRect();
 
     overlapping =
         box1.left < box2.right &&
@@ -15,73 +18,68 @@ function checkFlaskCollision() {
         box1.top < box2.bottom &&
         box1.bottom > box2.top;
 
+
+    // Show dialog box if character is overlapping laboratory
     if (overlapping) {
         popup.style.visibility = "visible";
-    } else {
+    }
+    else {
         popup.style.visibility = "hidden";
     }
 }
 
-document.addEventListener("keydown", (event) => {
-   
 
-    // Move UP
-    if (
-        event.key === "w" ||
-        event.key === "W" ||
-        event.key === "ArrowUp"
-    ) {
-        gsap.to(character2, {
+// Hide dialog box when page starts
+popup.style.visibility = "hidden";
+
+
+
+//keydown functions
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === 'w' ||
+        event.key === 'W' ||
+        event.key === 'ArrowUp') {
+        gsap.to("#character", {
             y: "-=30",
             duration: 0.5,
-            onComplete: checkFlaskCollision //checks collision after each displacment
+            onComplete: checkCollision //checks collision after each displacment 
         });
-
     }
-
-    // Move DOWN
-    else if (
-        event.key === "s" ||
-        event.key === "S" ||
-        event.key === "ArrowDown"
-    ) {
-       
-        gsap.to(character2, {
+    else if (event.key === 's' ||
+        event.key === 'S' ||
+        event.key === 'ArrowDown') {
+        gsap.to("#character", {
             y: "+=30",
             duration: 0.5,
-            onComplete: checkFlaskCollision //checks collision after each displacment
+            onComplete: checkCollision //checks collision after each displacment 
         });
-
     }
-
-    // Move LEFT
-    else if (
-        event.key === "a" ||
-        event.key === "A" ||
-        event.key === "ArrowLeft"
-    ) {
-       
-        gsap.to(character2, {
+    else if (event.key === 'a' ||
+        event.key === 'A' ||
+        event.key === 'ArrowLeft') {
+        gsap.to("#character", {
             x: "-=30",
             duration: 0.5,
-            onComplete: checkFlaskCollision //checks collision after each displacment
+            onComplete: checkCollision //checks collision after each displacment 
         });
-
     }
-
-    // Move RIGHT
-    else if (
-        event.key === "d" ||
-        event.key === "D" ||
-        event.key === "ArrowRight"
-    ) {
-
-        gsap.to(character2, {
+    else if (event.key === 'd' ||
+        event.key === 'D' ||
+        event.key === 'ArrowRight') {
+        gsap.to("#character", {
             x: "+=30",
             duration: 0.5,
-            onComplete: checkFlaskCollision //checks collision after each displacment
+            onComplete: checkCollision //checks collision after each displacment 
         });
-
+    }
+    //page switch
+    else if ((event.key === 'e' || event.key === 'E')
+        && overlapping === true) {
+        window.location.href = "laboratory.html";
     }
 
 });
+
+
+
